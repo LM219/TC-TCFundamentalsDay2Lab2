b3adebad058221d83d83dce18194b66a0d64a2d9
Louise Milligan <louise.milligan@hotmail.co.uk>
2017-08-10 17:24:34 +0100

    Finished Lab

--- src/main/java/io/zipcoder/fundamentals/day2/SumOfNumbers.java
+++ src/main/java/io/zipcoder/fundamentals/day2/SumOfNumbers.java
@@ -1,9 +1,27 @@
 package io.zipcoder.fundamentals.day2;
 
+import java.util.Scanner;
 
 public class SumOfNumbers {
 
     public static void main(String[] args){
+    	
+    	Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Pick a number, any number, apart from 13....?");
+        
+        int number = scanner.nextInt();
+        
+        if (number!= 13) {
+        for (int x=1; x<=number; x++)
+        {
+        	System.out.print(x + " ");
+        }
+        }
+        else {
+        	System.out.println("Whyyyy did you print my unlucky number! -_-");
+        }
+        
 
     }
 }
